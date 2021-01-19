79ea1bc5c7dfd2d4713ae7c1257e74d35cc3c247
Eli Pousson <eli.pousson@gmail.com>
2021-01-18 21:55:40 -0500

    Remove metro area support from geo_printout

--- R/geo_printout.R
+++ R/geo_printout.R
@@ -1,5 +1,5 @@
 # make geography printout--not exported
-geo_printout <- function(neighborhoods, towns, regions, counties, state, msa = FALSE, us = FALSE, new_england = FALSE) {
+geo_printout <- function(neighborhoods, towns, regions, counties, state, msa, us = FALSE) {
 
   out <- list(unique(neighborhoods), towns, names(regions), counties, state) %>%
     purrr::map2(c("Neighborhoods", "Towns", "Regions", "Counties", "State"), function(geo, geo_head) {
@@ -14,13 +14,7 @@ geo_printout <- function(neighborhoods, towns, regions, counties, state, msa = F
     }) %>%
     purrr::discard(function(x) is.null(x)) %>%
     as.character()
-  if (msa) {
-    if (new_england) {
-      out <- c(out, "MSA: All in New England")
-    } else {
-      out <- c(out, "MSA: All in US")
-    }
-  }
+    
   if (us) {
     out <- c(out, "US: Yes")
   }
